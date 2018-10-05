f1f5ad92890f3921236b77ca62406214b420c985
Raphael <ralpha19@hotmail.com>
2018-10-05 02:41:04 +0000

    WHILE and FOR loop to use POWER of a number

--- power.js
+++ power.js
@@ -1,9 +1,22 @@
+
+//WHILE LOOP
 let result = 1;
 let counter = 0;
 
 while (counter < 10) {
   result = result * 2;
   counter = counter + 1;
+}
+
+console.log(result);
+
+
+
+//FOR LOOP
+let result = 1;
+
+for (let counter = 0; counter < 10; counter +=1) {
+  result = result * 2;
 }
 
 console.log(result);
